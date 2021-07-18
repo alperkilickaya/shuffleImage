2b778788e5432c46a17ee2eba3f9cc54afb976c3
alperkilickaya <48261006+alperkilickaya@users.noreply.github.com>
2021-07-18 12:53:54 +0300

    yorum düzeltildi

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ const shuffle = document.querySelector('.btn-primary');
 const image = document.querySelector('.card-img-top');
 const card = document.querySelector('.card');
 const cardTitle = document.querySelector('.card-title');
-var index = 0;
-var prev; // önceki index'i tutacak
+var index = 0; // başlangıç index'i. Sayfa yüklendiğinde bunu kullanacağım.
+var prev; // önceki index'i tutacak.
 
 
 
@@ -38,12 +38,14 @@ shuffle.addEventListener('click',function(){
     console.log("Önceki İndex:",prev); // önceki index'i yaz. Kontrol amaçlı
 
     do {
-        index = Math.floor(Math.random()*3);//0-2 arası (2 dahil)yeni bir index oluştur.
+        index = Math.floor(Math.random()*3); //0-2 arası (0 ve 2 dahil) yeni bir index oluştur.
 
-    }while(prev == index) // önceki index üretilen index'e eşit olduğu sürece DO ile index üret
-    console.log("Üretilen index:",index); // üretilen index'i yaz
+    }while(prev == index) /* önceki index üretilen index'e eşit olduğu sürece DO ile index üret. 
+                            Bu sayede önceki index ile üretilen index'İn farklı olduğundan emin oldum */
+    
+    console.log("Üretilen index:",index); // üretilen index'i yaz. Kontrol amaçlı.
 
-    prev = index; // üretilen index'i karşılaştırma için prev'de sakla.
+    prev = index; // üretilen index'i bir sonraki karşılaştırma için prev'de sakla.
     
     /* 
     // burası iptal. 
@@ -55,8 +57,7 @@ shuffle.addEventListener('click',function(){
     
 // random index'i foto gösterme fonksiyonuna gönder.
     show(index);// index'i resim gösterme fonkiyonuna gönder
-    
-  
+      
 })
 
 function show(index){
